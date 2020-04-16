6b68f1e5f7d24404e6265dbc40c58d9b6f8aa568
NatanSantos <natansantos88@hotmail.com>
2020-04-16 00:02:16 -0300

    Build

---
+++ precache-manifest.f19967b11757c3c83b2a75bda17a934a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbeee32313b7bb8005bbafb35a9d1ad2",
+    "url": "/valor-moeda-react/index.html"
+  },
+  {
+    "revision": "d38a51849c25ddc0efde",
+    "url": "/valor-moeda-react/static/css/main.0e131765.chunk.css"
+  },
+  {
+    "revision": "4b3c816149e937f4c3f7",
+    "url": "/valor-moeda-react/static/js/2.183c4eae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/valor-moeda-react/static/js/2.183c4eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38a51849c25ddc0efde",
+    "url": "/valor-moeda-react/static/js/main.1af96170.chunk.js"
+  },
+  {
+    "revision": "c759dca9db0db91ce6e2",
+    "url": "/valor-moeda-react/static/js/runtime-main.07d98067.js"
+  }
+]);
